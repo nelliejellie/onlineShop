94a0134f71a9f2a4f62c8c69f02c0fe2fafe6cd6
emeka ewelike <emekaewelike@gmail.com>
2020-06-03 00:22:07 +0100

    added a readme among other stuffs

--- cart/cart.py
+++ cart/cart.py
@@ -35,3 +35,32 @@ class Cart(object):
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+
+    def __iter__(self):
+        """
+        iterate over the items in the cart and get the products from the
+        database
+        """
+        products_ids = self.cart.keys()
+        # get the product objects and add them to te cart
+        products = Product.objects.filter(id__in=products_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        # count all the items in the cart
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def get_total_price(self):
+        return sum(Decimal(item['price'])* item['quantity'] for item in self.cart.values())
+
+    def clear(self):
+        # remove cart from session
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
